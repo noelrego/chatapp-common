131ee0d703b97ef979a7ecc5c3b2c66e926060ac
noelrego <noelrego.nnr@gmail.com>
2024-09-27 21:07:45 +0530

    1.0.1

--- src/authuser/authuser.enum.ts
+++ src/authuser/authuser.enum.ts
@@ -1,5 +1,6 @@
-export enum MsgPatternAuthUserService  {
+export enum N_MsgPatternAuthUserService  {
     GET_ALL_USERS = 'get_all_users',
     REGISTER_USER = 'register_user',
-    LOGIN_USER = 'login_user'
+    LOGIN_USER = 'login_user',
+    CHECK_USERNAME = 'check_username'
 }
